54e30c9028e86768367522696b7d6cc0d4387645
Segaran <blsgrn@tutanota.com>
2023-03-09 10:57:49 +0530

    printf examples

---
+++ src/package01/Printfnumbers.java
@@ -0,0 +1,14 @@
+package package01;
+
+public class Printfnumbers {
+  public static void main(String[] args) {
+    int num = 42;
+    System.out.printf("The value of num is %d\n", num); // Output: The value of num is 42
+
+    float pi = 3.14159265359f;
+    System.out.printf("The value of pi is approximately %.2f\n", pi); // Output: The value of pi is approximately 3.14
+
+    int num1 = 8;
+    System.out.printf("The octal representation of %d is %o\n", num1, num1); // Output: 10
+  }
+}
